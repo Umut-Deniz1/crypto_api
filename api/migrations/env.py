dce0b891469e878e485867933b3e019da0f34a47
Umut Deniz <umut.deniz@monotech.group>
2024-07-17 15:04:39 +0200

    fix: pre-commit

--- api/migrations/env.py
+++ api/migrations/env.py
@@ -1,9 +1,7 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,6 +18,7 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 
 from api.models import Base
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -27,10 +26,12 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+
 def get_url():
     from api.db.database import SQLALCHEMY_DATABASE_URL
 
     return SQLALCHEMY_DATABASE_URL
+
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
